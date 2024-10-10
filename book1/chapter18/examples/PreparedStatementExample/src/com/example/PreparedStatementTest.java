00846e28443f5b1661f9ec6e69e9b6f16091263e
Fezao187 <katlegomaredi9@gmail.com>
2024-10-11 00:17:43 +0200

    Lesson 18 connect to postgresql

--- book1/chapter18/examples/PreparedStatementExample/src/com/example/PreparedStatementTest.java
+++ book1/chapter18/examples/PreparedStatementExample/src/com/example/PreparedStatementTest.java
@@ -13,14 +13,14 @@ import java.sql.SQLException;
 public class PreparedStatementTest {
 
     public static void main(String[] args) {
-        String url = "jdbc:postgresql://192.168.1.170:5432/sample?ssl=true";
-        String username = "tiger";
-        String password = "scott";
+        String url = "jdbc:postgresql://localhost:5432/employee_db";
+        String username = "postgres";
+        String password = "Kristine";
         String input = "";
         double searchValue;
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         try (Connection con = DriverManager.getConnection(url, username, password)) {
-            PreparedStatement pStmt = con.prepareStatement("SELECT * FROM Employee WHERE Salary > ?");
+            PreparedStatement pStmt = con.prepareStatement("SELECT * FROM employess WHERE salary_id > ?");
             // Loop 
             while (true) {
                 System.out.print("Enter salary to search for or Q to quit: ");
@@ -32,16 +32,15 @@ public class PreparedStatementTest {
                 pStmt.setDouble(1, searchValue);
                 ResultSet rs = pStmt.executeQuery();
                 while (rs.next()) {
-                    int empID = rs.getInt("ID");
-                    String first = rs.getString("FIRSTNAME");
-                    String last = rs.getString("LASTNAME");
-                    Date birth_date = rs.getDate("BIRTHDATE");
-                    float salary = rs.getFloat("SALARY");
+                    int empID = rs.getInt("emp_id");
+                    String first = rs.getString("first_name");
+                    String last = rs.getString("surname");
+                    float salary = rs.getFloat("salary_id");
                     System.out.format("Employee ID:   %s%n"
                             + "Employee Name: %s %s%n"
                             + "Birth Date:    %s%n"
                             + "Salary:        %s%n%n",
-                            empID, first, last, birth_date, salary);
+                            empID, first, last, salary);
                 }
             }// end of while
         } catch (NumberFormatException n) {
