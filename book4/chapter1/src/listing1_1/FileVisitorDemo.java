abf39f8423b8486a8beb354c070777d679948a9c
Fezao187 <katlegomaredi9@gmail.com>
2024-10-15 21:51:53 +0200

    Book 4, chapter 1

---
+++ book4/chapter1/src/listing1_1/FileVisitorDemo.java
@@ -0,0 +1,41 @@
+package listing1_1;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+/**
+ * This program lists all the files in C:\Windows\System32,
+ * including all its subfolders.
+ */
+public class FileVisitorDemo {
+    public static void main(String[] args) {
+        Path start = Paths.get("c:/Windows/System32");
+        MyFileVisitor visitor = new MyFileVisitor();
+        try {
+            Files.walkFileTree(start, visitor);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static class MyFileVisitor extends SimpleFileVisitor<Path> {
+        public FileVisitResult visitFile(Path file,
+                                         BasicFileAttributes attr) {
+            System.out.println(file.toString());
+            return FileVisitResult.CONTINUE;
+        }
+
+        public FileVisitResult visitFileFailed(Path file,
+                                               BasicFileAttributes attr) {
+            System.out.println(file.toString() + " COULD NOT ACCESS!");
+            return FileVisitResult.CONTINUE;
+        }
+
+        public FileVisitResult preVisitDirectoryFailed
+                (Path dir, IOException e) {
+            System.out.println(dir.toString() + " COULD NOT ACCESS!");
+            return FileVisitResult.CONTINUE;
+        }
+    }
+}
