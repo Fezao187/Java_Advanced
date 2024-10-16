97625ee1254ab5a07323f2a27b6fd365ca03d4f9
Fezao187 <katlegomaredi9@gmail.com>
2024-10-16 12:17:51 +0200

    Book 6, chapter 2

---
+++ book3/chapter2/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module chapter2.chapter2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens chapter2.chapter2 to javafx.fxml;
+    exports chapter2.chapter2;
+}
