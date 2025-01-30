7d8da1d41982cde6f9b8bd12b7631f0a7bbcaada
Fezao187 <katlegomaredi9@gmail.com>
2025-01-30 13:48:20 +0200

    Arrays

---
+++ tutorials/data_structures_&&_algos/src/arrays/ArrayExample.java
@@ -0,0 +1,24 @@
+package arrays;
+
+public class ArrayExample {
+    public static void main(String[] args){
+        // Create array of strings
+        String[] groceryItems={"Apples","Oranges", "Bananas","Cucumbers","Chips"};
+
+        // Iterate through the array
+        for(int i=0; i< groceryItems.length;i++){
+            System.out.print(groceryItems[i] +", ");
+        }
+
+        // Find an element
+        System.out.println("\nindex 3: "+groceryItems[3]);
+
+        // remove element
+        groceryItems[3]=null;
+
+        System.out.println("Removed item: "+ groceryItems[3]);
+
+        // Check length
+        System.out.println("Number of groceries: "+ groceryItems.length);
+    }
+}
