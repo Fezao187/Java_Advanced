b169f3bdac6e078d69679c939c6f95041fa8ee5a
Fezao187 <katlegomaredi9@gmail.com>
2025-01-31 09:01:34 +0200

    Sets

---
+++ tutorials/data_structures_&&_algos/src/data_structures/sets/Example.java
@@ -0,0 +1,19 @@
+package data_structures.sets;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Example {
+    public static void main(String[] args) {
+        Set<Ball> balls = new HashSet<>();
+        balls.add(new Ball("blue"));
+        balls.add(new Ball("blue"));
+        balls.add(new Ball("yellow"));
+        balls.add(new Ball("red"));
+        balls.remove(new Ball("red"));
+        System.out.println(balls.size());
+        balls.forEach(System.out::println);
+    }
+
+    record Ball(String color){}
+}
