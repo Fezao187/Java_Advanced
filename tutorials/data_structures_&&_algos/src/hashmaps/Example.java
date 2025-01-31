743593e66ee8a90ee4c5998cd959c4f6a875029c
Fezao187 <katlegomaredi9@gmail.com>
2025-01-31 06:53:58 +0200

    Hash map

---
+++ tutorials/data_structures_&&_algos/src/hashmaps/Example.java
@@ -0,0 +1,60 @@
+package hashmaps;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Example {
+    public static void main(String[] args) {
+        // Create a hashmap
+        HashMap<String, List<String>> countries = new HashMap<String, List<String>>();
+
+        // Create a list of counties
+        List<String> G = new ArrayList<String>(){
+            {
+                add("Ghana");
+                add("GreenLand");
+                add("Greece");
+            }
+        };
+
+        List<String> I = new ArrayList<String>(){
+            {
+                add("India");
+                add("Ireland");
+                add("Iran");
+                add("Iraq");
+                add("Italy");
+            }
+        };
+
+        List<String> U = new ArrayList<String>(){
+            {
+                add("United States");
+                add("United Kingdom");
+                add("Uruguay");
+            }
+        };
+
+        // Initialise hashmap
+        countries.put("G", G);
+        countries.put("I", I);
+        countries.put("U", U);
+
+        // get a list of I's
+        List<String> result = countries.get("I");
+
+        for(String country: result){
+            System.out.println(country);
+        }
+
+        // remove key value
+        countries.remove("I");
+        System.out.println();
+
+        // Try to get I
+        System.out.println("Trying to get I");
+        System.out.println(countries.get("I"));
+
+    }
+}
