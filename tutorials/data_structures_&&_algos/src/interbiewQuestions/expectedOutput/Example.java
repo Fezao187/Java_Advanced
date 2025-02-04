ff80bd4728090bc1c20ea5dfefea49a1af64c44f
Fezao187 <katlegomaredi9@gmail.com>
2025-02-04 21:08:15 +0200

    Code snippet

---
+++ tutorials/data_structures_&&_algos/src/interbiewQuestions/expectedOutput/Example.java
@@ -0,0 +1,8 @@
+package interbiewQuestions.expectedOutput;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println(100+100+"KeepCoding");
+        System.out.println("KeepCoding"+100+100);
+    }
+}
