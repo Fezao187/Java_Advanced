72baeefaec8bdcde31999bc670c6b95f1018a42b
Fezao187 <katlegomaredi9@gmail.com>
2025-02-04 22:35:29 +0200

    Singleton

---
+++ tutorials/data_structures_&&_algos/src/interbiewQuestions/singltonClass/Main.java
@@ -0,0 +1,10 @@
+package interbiewQuestions.singltonClass;
+
+public class Main {
+    public static void main(String[] args) {
+        Singleton singleton1=Singleton.getSingleton();
+        Singleton singleton2=Singleton.getSingleton();
+        System.out.println(singleton1);
+        System.out.println(singleton2);
+    }
+}
---
+++ tutorials/data_structures_&&_algos/src/interbiewQuestions/singltonClass/Singleton.java
@@ -0,0 +1,11 @@
+package interbiewQuestions.singltonClass;
+
+public class Singleton {
+    private static Singleton singleton=new Singleton();
+    private Singleton(){}
+
+    public static Singleton getSingleton(){
+        return singleton;
+    }
+
+}
