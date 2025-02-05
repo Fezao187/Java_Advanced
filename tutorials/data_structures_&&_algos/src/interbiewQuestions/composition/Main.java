791d32aed30bf3f585e065a34f9d93887ba66b7e
Fezao187 <katlegomaredi9@gmail.com>
2025-02-05 09:35:27 +0200

    Composition

--- tutorials/data_structures_&&_algos/src/interbiewQuestions/composition/Main.java
+++ tutorials/data_structures_&&_algos/src/interbiewQuestions/composition/Main.java
@@ -3,5 +3,11 @@ package interbiewQuestions.composition;
 public class Main {
     public static void main(String[] args) {
         Person p = new Person();
+
+        System.out.println("I am the main block");
+    }
+
+    static {
+        System.out.println("I am the static block");
     }
 }
